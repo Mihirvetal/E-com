341649b6e3fd32e18e6c6cbc9648cff29dd1e729
Mihir Vetal <151329052+Mihirvetal@users.noreply.github.com>
2025-01-30 02:50:39 +0530

    Done

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -29,7 +29,7 @@ const SignUp = () => {
   return (
     <div className="flex h-266">
       {/* Image section with 4/5 width */}
-      <div className="flex-4 bg-cover bg-center" style={{ backgroundImage: "url('https://plus.unsplash.com/premium_photo-1734713078804-818d9e818c9c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2MXx8fGVufDB8fHx8fA%3D%3D')" }}></div>
+      <div className="flex-3 bg-cover bg-center" style={{ backgroundImage: "url('https://plus.unsplash.com/premium_photo-1734713078804-818d9e818c9c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2MXx8fGVufDB8fHx8fA%3D%3D')" }}></div>
       
       {/* Sign Up form section with 1/5 width */}
       <div className="flex-1 flex items-center justify-center ">
